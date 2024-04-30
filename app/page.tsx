962422816d9109ef22093eb455066ff515643e80
NJF289 <122659161+NJF289@users.noreply.github.com>
2024-04-30 16:08:15 +0000

    rwetgudf

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 
 import Header from "./componentes/Header";
+import Footer from "./componentes/Footer";
 
 export default function Home() {
   return (
     <div>
       <Header/> 
+      <Footer/>
       </div>
 );
 }
